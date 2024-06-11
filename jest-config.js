252d01615ff85c931ea36e4472569c50ec89d3e5
Checo894 <A01068505@tec.mx>
2024-06-11 15:54:00 -0600

    pruebas db y db_chatbot LISTAS 2

---
+++ jest-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    testMatch: ['**/tests/**/*.test.js'],
+  };
+  
+
+// module.exports = {
+//     testEnvironment: 'node',
+//     testMatch: ['**/tests/**/*.test.js']
+//   };
+  
